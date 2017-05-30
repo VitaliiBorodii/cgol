494320c2c20ff3a36d1601856c5d784ddc0c6a71
vitaliiborodii <allterraintacticalenforcer@gmail.com>
2017-05-30 13:04:57 +0300

    add DB cleanup

--- src/js/sagas/db.js
+++ src/js/sagas/db.js
@@ -5,6 +5,28 @@ import { ADD_PATTERN_DONE, TOGGLE_CELL_DONE } from '../constants/cells';
 import { NEXT_STEP_DONE } from '../constants/life';
 import { REQUEST_LAST_STEP, RESTORE_LIFE, REQUEST_PREVIOUS_STEP, RECEIVE_PREVIOUS_STEP, RESET_LIFE } from '../constants/lifecycles';
 
+const CLEANUP_INTERVAL = 10000;
+const CLEANUP_SIZE = 500;
+
+let interval;
+
+const cancelCleanup = () => {
+  clearInterval(interval);
+  interval = null;
+};
+
+const runCleanup = () => {
+  interval = setInterval(() => {
+    db.keys().then(r => {
+      if (r.length > CLEANUP_SIZE) {
+        const keysToDelete = r.slice(0, r.length - CLEANUP_SIZE);
+        keysToDelete.forEach(key => db.delete(key));
+        console.log(`Removing ${keysToDelete.length} keys`);
+      }
+    });
+  }, CLEANUP_INTERVAL);
+};
+
 const getCells = state => state.cells;
 
 function resetDb() {
@@ -39,6 +61,7 @@ function* dbSaga() {
   yield takeEvery([ADD_PATTERN_DONE, NEXT_STEP_DONE, TOGGLE_CELL_DONE], saveStep);
   yield takeEvery(REQUEST_LAST_STEP, restoreLife);
   yield takeEvery(REQUEST_PREVIOUS_STEP, previousStep);
+  runCleanup();
 }
 
 export default dbSaga;
